d111a172c66ce5877eb1aa6318317681ae5efbc9
Shafique <SHAFIQUL-IT@SHAFIQUL-IT.local>
2015-10-20 16:04:28 +0600

    first commit

---
+++ GitTestProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTestProject
+//
+//  Created by Shafique on 10/20/15.
+//  Copyright (c) 2015 Shafiqul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitTestProject/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GitTestProject
+//
+//  Created by Shafique on 10/20/15.
+//  Copyright (c) 2015 Shafiqul. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+    
+    NSLog(@"commited");
+}
+
+@end
